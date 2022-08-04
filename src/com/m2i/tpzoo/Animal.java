9f653fda4539858aa79ecaa70ddfc70f5b12a200
jcdominguez <jcd.pro@gmail.com>
2022-08-04 16:16:12 +0200

    Etape 1 - creation Animal

---
+++ src/com/m2i/tpzoo/Animal.java
@@ -0,0 +1,40 @@
+package com.m2i.tpzoo;
+
+public class Animal {
+
+	
+	private String nom;
+	private String cri;
+	
+	
+	public Animal() {
+		
+	}
+
+	public Animal(String nom, String cri) {
+		this.nom = nom;
+		this.cri = cri;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	public String getCri() {
+		return cri;
+	}
+
+	public void setCri(String cri) {
+		this.cri = cri;
+	}
+
+	public String pousserCri() {
+		return this.cri;
+	}
+	
+}
--- src/com/m2i/tpzoo/TpZoo.java
+++ src/com/m2i/tpzoo/TpZoo.java
@@ -5,6 +5,9 @@ public class TpZoo {
 	public static void main(String[] args) {
 		
 		System.out.println("TP ZOO");
+		
+		Animal animal = new Animal("Médor", "Ouaf");
+		System.out.println(animal.getCri());
 
 	}
 
